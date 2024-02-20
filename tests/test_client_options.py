11256cce0909bcc1075fac16ad1217450e3b2912
Harish Navnit <harishnavnit@gmail.com>
2024-02-20 16:51:16 +0800

    WIP: Add more tests

--- tests/test_client_options.py
+++ tests/test_client_options.py
@@ -1,43 +1,38 @@
+
+import unittest
 from gotrue import SyncMemoryStorage
 
 from supabase.lib.client_options import ClientOptions
 
 
-class TestClientOptions:
-    def test_replace_returns_updated_options(self):
-        storage = SyncMemoryStorage()
-        storage.set_item("key", "value")
-        options = ClientOptions(
+class TestClientOptions(unittest.TestCase):
+    def setUp(self) -> None:
+        self.storage = SyncMemoryStorage()
+        self.storage.set_item("key", "value")
+        self.options = ClientOptions(
             schema="schema",
             headers={"key": "value"},
             auto_refresh_token=False,
             persist_session=False,
-            storage=storage,
-            realtime={"key": "value"},
-        )
+            storage=self.storage,
+            realtime={"key": "value"
+        })
 
-        actual = options.replace(schema="new schema")
-        expected = ClientOptions(
-            schema="new schema",
-            headers={"key": "value"},
-            auto_refresh_token=False,
-            persist_session=False,
-            storage=storage,
-            realtime={"key": "value"},
+    def test_replace_returns_updated_options(self) -> None:
+        self.assertEqual(
+            self.options.replace(schema="new schema"),
+            ClientOptions(
+                schema="new schema",
+                headers={"key": "value"},
+                auto_refresh_token=False,
+                persist_session=False,
+                storage=self.storage,
+                realtime={"key": "value"},
+            )
         )
-
-        assert actual == expected
 
     def test_replace_updates_only_new_options(self):
-        # Arrange
-        storage = SyncMemoryStorage()
-        storage.set_item("key", "value")
-        options = ClientOptions(storage=storage)
-        new_options = options.replace()
-
-        # Act
-        new_options.storage.set_item("key", "new_value")
-
-        # Assert
-        assert options.storage.get_item("key") == "new_value"
-        assert new_options.storage.get_item("key") == "new_value"
+        modified = self.options.replace()
+        modified.storage.set_item("key", "new_value")
+        self.assertEqual(modified.storage.get_item("key"), "new_value")
+        self.assertEqual(self.options.storage.get_item("key"), "new_value")
