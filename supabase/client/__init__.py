624429525781d888d09f863f59f05c9c451e77db
Harish Navnit <harishnavnit@gmail.com>
2024-02-20 13:52:31 +0800

    remove redundant client.py

---
+++ supabase/client/__init__.py
@@ -0,0 +1,26 @@
+
+from postgrest import APIError as PostgrestAPIError
+from postgrest import APIResponse as PostgrestAPIResponse
+from storage3.utils import StorageException
+
+
+from ..__version__ import __version__
+from ..lib.realtime_client import SupabaseRealtimeClient
+from .auth import AuthClient
+from .default import ClientOptions, create_client
+from .default import SupabaseClient
+from .default import SyncStorageClient as SupabaseStorageClient
+
+
+__all__ = [
+    "__version__",
+    "create_client",
+    "AuthClient",
+    "ClientOptions",
+    "PostgrestAPIError",
+    "PostgrestAPIResponse",
+    "StorageException",
+    "SupabaseClient",
+    "SupabaseRealtimeClient",
+    "SupabaseStorageClient"
+]
