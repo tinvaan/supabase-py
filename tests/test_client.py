6a22f92d505d8587c0e88704662d7ded90edbbd8
Harish Navnit <harishnavnit@gmail.com>
2024-02-27 20:17:09 +0800

    Begin porting default client tests to unittest

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,17 +1,23 @@
-from __future__ import annotations
 
-from typing import Any
+import os
+import unittest
 
-import pytest
+from supabase import create_client, SupabaseClient
+from supabase.client.exceptions import ConfigurationError
 
 
-@pytest.mark.xfail(
-    reason="None of these values should be able to instantiate a client object"
-)
-@pytest.mark.parametrize("url", ["", None, "valeefgpoqwjgpj", 139, -1, {}, []])
-@pytest.mark.parametrize("key", ["", None, "valeefgpoqwjgpj", 139, -1, {}, []])
-def test_incorrect_values_dont_instantiate_client(url: Any, key: Any) -> None:
-    """Ensure we can't instantiate client with invalid values."""
-    from supabase import SupabaseClient, create_client
+class TestDefaultClient(unittest.TestCase):
+    def setUp(self) -> None:
+        self.url = os.getenv("SUPABASE_URL")
+        self.key = os.getenv("SUPABASE_KEY")
+        self.client = SupabaseClient(self.url, self.key)
 
-    _: SupabaseClient = create_client(url, key)
+    def test_invalid_create_client(self):
+        urls = ("", None, "valeefgpoqwjgpj", 139, -1, {}, [])
+        keys = ("", None, "valeefgpoqwjgpj", 139, -1, {}, [])
+        for url in urls:
+            for key in keys:
+                try:
+                    self.assertIsNone(create_client(url, key))
+                except ConfigurationError:
+                    self.assertTrue(1 == 1)
