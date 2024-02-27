5448f4de1399a3ea3dbf255a7113f07ca9fe5ac6
Harish Navnit <harishnavnit@gmail.com>
2024-02-27 19:54:00 +0800

    Move test function configuration class to unittest style

--- tests/test_function_configuration.py
+++ tests/test_function_configuration.py
@@ -1,15 +1,19 @@
 
+import unittest
+
 from supabase import SupabaseClient
 
 
-def test_functions_client_initialization() -> None:
-    ref = "ooqqmozurnggtljmjkii"
-    url = f"https://{ref}.supabase.co"
-    # Sample JWT Key
-    key = "xxxxxxxxxxxxxx.xxxxxxxxxxxxxxx.xxxxxxxxxxxxxxx"
-    sp = SupabaseClient(url, key)
-    assert sp.functions_url == f"https://{ref}.supabase.co/functions/v1"
+class TestFunctionsClient(unittest.TestCase):
+    def setUp(self) -> None:
+        self.ref = "ooqqmozurnggtljmjkii"
+        self.url = f"https://{self.ref}.supabase.co"
+        self.key = "xxxxxxxxxxxxxx.xxxxxxxxxxxxxxx.xxxxxxxxxxxxxxx"
+        self.client = SupabaseClient(self.url, self.key)
 
-    url = "https://localhost:54322"
-    sp_local = SupabaseClient(url, key)
-    assert sp_local.functions_url == f"{url}/functions/v1"
+    def test_functions_client_initialization(self):
+        assert self.client.functions_url == f"https://{self.ref}.supabase.co/functions/v1"
+
+        url = "https://localhost:54322"
+        client = SupabaseClient(url, self.key)
+        assert client.functions_url == f"{url}/functions/v1"
