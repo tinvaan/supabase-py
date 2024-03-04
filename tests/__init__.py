264c50c1abc15b520896abfb5c697bd47c40a3d2
Harish Navnit <harishnavnit@gmail.com>
2024-03-04 17:38:09 +0800

    more test cases

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,17 +2,26 @@
 import os
 import unittest
 
+from supabase import SupabaseClient
+from supabase.lib.client_options import ClientOptions
+
 
 class ClientTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.rows = []
         cls.table = "countries"
         cls.url = os.getenv("SUPABASE_URL")
         cls.key = os.getenv("SUPABASE_KEY")
         cls.schema = os.getenv("SUPABASE_TEST_SCHEMA", "public")
+        cls.countries = ["Argentina", "Brazil", "Canada", "Dubai"]
+
+    def setUp(self):
+        self.opts = ClientOptions(schema=self.schema)
+        self.client = SupabaseClient.create(self.url, self.key, options=self.opts)
+        for country in self.countries:
+            self.client.table(self.table).insert({"name": country}).execute()
 
     def tearDown(self):
-        for row in self.rows:
-            self.client.table(self.table).delete().eq("id", row.get('id')).execute() # pylint: disable=no-member
-        self.rows.clear()
+        for country in self.countries:
+            self.client.table(self.table).delete().eq("name", country).execute()
+        self.countries.clear()
