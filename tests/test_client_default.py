efe8b6ce748ac768cf354a71ec7473a4c36ccc65
Harish Navnit <harishnavnit@gmail.com>
2024-03-05 10:34:25 +0800

    Fix client test setup states

--- tests/test_client_default.py
+++ tests/test_client_default.py
@@ -5,9 +5,8 @@ from postgrest.exceptions import APIError
 
 from supabase import SupabaseClient, create_client
 from supabase.client.exceptions import ConfigurationError
-from supabase.lib.client_options import ClientOptions
 
-from . import ClientTest
+from . import AsyncTestClient, TestClient
 
 
 def test_create_client():
@@ -20,7 +19,7 @@ def test_create_client():
     assert create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
 
 
-class TestSync(ClientTest):
+class TestSync(TestClient):
     def test_create(self):
         for url in ("", None, "valeefgpoqwjgpj", 139, -1, {}, []):
             for key in ("", None, "valeefgpoqwjgpj", 139, -1, {}, []):
@@ -41,7 +40,7 @@ class TestSync(ClientTest):
         with self.assertRaises(APIError):
             self.client.table("foobar").insert({"name": "barfoo"}).execute()
 
-        self.countries.append("Wadiya")
+        self.countries.add("Wadiya")
         op = self.client.table(self.table).insert({"name": "Wadiya"}).execute()
         self.assertIsNotNone(op)
         for d in op.data:
@@ -51,31 +50,22 @@ class TestSync(ClientTest):
         with self.assertRaises(APIError):
             self.client.table("foobar").select().eq("id", 1).execute()
 
-        op = self.client.table(self.table).select("*").eq("name", "Canada").execute()
-        self.assertIsNotNone(op)
-        self.assertEqual(len(op.data), 1)
-
         op = self.client.table(self.table).select("*").execute()
         self.assertIsNotNone(op)
         self.assertEqual(len(op.data), len(self.countries))
 
+        op = self.client.table(self.table).select("*").eq("name", "Canada").execute()
+        self.assertIsNotNone(op)
+        self.assertEqual(len(op.data), 1)
+
     def test_table_update(self):
         with self.assertRaises(APIError):
             self.client.table("foobar").update({"foo": "bar"}).execute()
-            self.client.table(self.table).update({"foo": "bar"}).eq(
-                "name", "Canada"
-            ).execute()
-            self.client.table(self.table).update({"name": "Australia"}).eq(
-                "foo", "bar"
-            ).execute()
+            self.client.table(self.table).update({"foo": "bar"}).eq("name", "Canada").execute()
+            self.client.table(self.table).update({"name": "Australia"}).eq("foo", "bar").execute()
 
-        self.countries.append("Denmark")
-        op = (
-            self.client.table(self.table)
-            .update({"name": "Denmark"})
-            .eq("name", "Dubai")
-            .execute()
-        )
+        self.countries.add("Denmark")
+        op = self.client.table(self.table).update({"name": "Denmark"}).eq("name", "Dubai").execute()
         self.assertIsNotNone(op)
         for d in op.data:
             self.assertEqual(d.get("name"), "Denmark")
@@ -92,82 +82,60 @@ class TestSync(ClientTest):
         pass
 
 
-class TestAsync(ClientTest):
-    def setUp(self):
-        super().setUp()
-        self.opts = ClientOptions(schema=self.schema, is_async=True)
-        self.aclient = SupabaseClient.create(self.url, self.key, options=self.opts)
-
+class TestAsync(AsyncTestClient):
     def test_create(self):
         for url in ("", None, "valeefgpoqwjgpj", 139, -1, {}, []):
             for key in ("", None, "valeefgpoqwjgpj", 139, -1, {}, []):
                 with self.assertRaises(ConfigurationError):
                     SupabaseClient.create(url, key, options=self.opts)
-        self.assertIsNotNone(
-            SupabaseClient.create(self.url, self.key, options=self.opts)
-        )
+        self.assertIsNotNone(SupabaseClient.create(self.url, self.key, options=self.opts))
 
     async def test_table_delete(self):
         with self.assertRaises(APIError):
-            await self.aclient.table("foobar").delete().eq("id", 1).execute()
+            await self.client.table("foobar").delete().eq("id", 1).execute()
 
-        op = (
-            await self.aclient.table(self.table).delete().eq("name", "Canada").execute()
-        )
+        op = await self.client.table(self.table).delete().eq("name", "Canada").execute()
         self.assertIsNotNone(op)
         for d in op.data:
             self.assertEqual(d.get("name"), "Canada")
 
     async def test_table_insert(self):
         with self.assertRaises(APIError):
-            await self.aclient.table("foobar").insert({"name": "barfoo"}).execute()
+            await self.client.table("foobar").insert({"name": "barfoo"}).execute()
 
-        op = await self.aclient.table(self.table).insert({"name": "Wadiya"}).execute()
+        self.countries.add("Wadiya")
+        op = await self.client.table(self.table).insert({"name": "Wadiya"}).execute()
         self.assertIsNotNone(op)
         for d in op.data:
             self.assertEqual(d.get("name"), "Wadiya")
 
     async def test_table_select(self):
         with self.assertRaises(APIError):
-            await self.aclient.table("foobar").select().eq("id", 1).execute()
+            await self.client.table("foobar").select().eq("id", 1).execute()
 
-        op = (
-            await self.aclient.table(self.table)
-            .select("*")
-            .eq("name", "Canada")
-            .execute()
-        )
-        self.assertIsNotNone(op)
-        self.assertEqual(len(op.data), 1)
-
-        op = await self.aclient.table(self.table).select("*").execute()
+        op = await self.client.table(self.table).select("*").execute()
         self.assertIsNotNone(op)
         self.assertEqual(len(op.data), len(self.countries))
 
+        op = await self.client.table(self.table).select("*").eq("name", "Canada").execute()
+        self.assertIsNotNone(op)
+        self.assertEqual(len(op.data), 1)
+
     async def test_table_update(self):
         with self.assertRaises(APIError):
-            await self.aclient.table("foobar").update({"foo": "bar"}).execute()
-            await self.aclient.table(self.table).update({"foo": "bar"}).eq(
-                "name", "Canada"
-            ).execute()
-            await self.aclient.table(self.table).update({"name": "Australia"}).eq(
-                "foo", "bar"
-            ).execute()
+            await self.client.table("foobar").update({"foo": "bar"}).execute()
+            await self.client.table(self.table).update({"foo": "bar"}).eq("name", "Canada").execute()
+            await self.client.table(self.table).update({"name": "Australia"}).eq("foo", "bar").execute()
 
-        self.countries.append("Denmark")
-        op = (
-            await self.aclient.table(self.table)
-            .update({"name": "Denmark"})
-            .eq("name", "Dubai")
-            .execute()
-        )
+        self.countries.add("Denmark")
+        op = await self.client.table(self.table).update({"name": "Denmark"}).eq("name", "Dubai").execute()
         self.assertIsNotNone(op)
         for d in op.data:
             self.assertEqual(d.get("name"), "Denmark")
 
     async def test_rpc(self):
         with self.assertRaises(APIError):
-            await self.aclient.rpc("dead", {"foo": "bar"}).execute()
+            await self.client.rpc("dead", {"foo": "bar"}).execute()
 
-        op = await self.aclient.rpc("alive", {}).execute()
+        op = await self.client.rpc("alive", {}).execute()
         self.assertTrue(op.data)
